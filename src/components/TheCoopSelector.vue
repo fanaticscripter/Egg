18872f92cd855fd30df385cf2ecc1c510a8cc3f2
fanaticscripter <fanaticscripter@gmail.com>
2021-04-27 16:46:38 +0800

    TheCoopSelector: set autocapitalize to off on input fields

--- src/components/TheCoopSelector.vue
+++ src/components/TheCoopSelector.vue
@@ -27,6 +27,7 @@
                 class="appearance-none block w-full px-3 py-2 text-base border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm text-gray-900 dark:text-gray-100 dark:bg-gray-700 dark:border-gray-500"
                 placeholder="Contract ID"
                 spellcheck="false"
+                autocapitalize="off"
                 v-model.trim="manuallyEnteredContractId"
               />
             </div>
@@ -53,6 +54,7 @@
               class="appearance-none block w-full px-3 py-2 text-base border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm text-gray-900 dark:text-gray-100 dark:bg-gray-700 dark:border-gray-500"
               placeholder="Coop Code"
               spellcheck="false"
+              autocapitalize="off"
               v-model.trim="coopCode"
             />
           </div>

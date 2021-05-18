19f9808288ca5b7a923df34b9c304b4be459749c
fanaticscripter <fanaticscripter@gmail.com>
2021-05-18 21:22:57 +0800

    Move eicoop/src/lib/eggs => lib/eggs

--- eicoop/src/lib/index.ts
+++ eicoop/src/lib/index.ts
@@ -3,7 +3,6 @@ export * from 'lib';
 export * from './contract';
 export * from './contractList';
 export * from './coop';
-export * from './eggs';
 export * from './rewards';
 export * from './role';
 export * from './time';
--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 export * from './api';
+export * from './eggs';
 export { ei, aux } from './proto';
 export * from './utils';

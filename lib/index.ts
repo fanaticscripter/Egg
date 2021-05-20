d0ae685cb9e1571bcd8c191b5060812d042119fa
fanaticscripter <fanaticscripter@gmail.com>
2021-05-20 11:21:57 +0800

    Move enlightenment/src/lib/time to lib/time

--- lib/index.ts
+++ lib/index.ts
@@ -2,4 +2,5 @@ export * from './api';
 export * from './eggs';
 export * from './prophecy_eggs';
 export { ei, aux } from './proto';
+export * from './time';
 export * from './utils';
--- wasmegg/enlightenment/src/lib/index.ts
+++ wasmegg/enlightenment/src/lib/index.ts
@@ -4,6 +4,5 @@ export * from './calculator';
 export * from './catalog';
 export * from './effects';
 export * from './farmcalc';
-export * from './time';
 export * from './units';
 export * from './utils';

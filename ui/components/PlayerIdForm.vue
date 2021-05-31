bc630916ad73c6ab2e774cd4abccb8e962fd3a59
fanaticscripter <fanaticscripter@gmail.com>
2021-05-31 23:42:13 +0800

    ui: fix eslint issues

--- ui/components/PlayerIdForm.vue
+++ ui/components/PlayerIdForm.vue
@@ -6,20 +6,20 @@
     <div>
       <label for="email" class="sr-only">Player ID</label>
       <input
-        type="text"
-        name="playerId"
         id="playerId"
         v-model.trim="playerId"
+        type="text"
+        name="playerId"
         class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
         placeholder="Player ID"
       />
       <div class="text-center">
         <span
-          class="mt-2 inline-flex items-center space-x-1"
           v-tippy="{
             content:
               'The ID asked for here is the unique ID used by Egg, Inc.\'s server to identify your account. You can find it in game screen -> nine dots menu -> Settings -> Privacy & Data, at the very bottom. It should look like EI1234567890123456. Your old game services ID prior to the Artifact Update does not work here. Also note that the ID is case-sensitive.',
           }"
+          class="mt-2 inline-flex items-center space-x-1"
         >
           <base-info />
           <span class="text-xs text-gray-500">Where do I find my ID?</span>

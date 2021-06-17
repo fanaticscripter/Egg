738800ceecd1da0309bb19d739125c72f39269b6
fanaticscripter <fanaticscripter@gmail.com>
2021-06-17 18:13:59 +0800

    wasmegg/_home: update donors

--- wasmegg/_home/src/donors.ts
+++ wasmegg/_home/src/donors.ts
@@ -2,7 +2,7 @@ type FixedLengthArray<T, L extends number> = [T, ...T[]] & { length: L };
 
 export const donors: FixedLengthArray<string, 30> = [
   'Dr. Tuen',
-  'WhaiFuji#4379',
+  'WhaiFuji',
   'DhrMekmek',
   'CW_Mikey',
   'Pete Tycoon',

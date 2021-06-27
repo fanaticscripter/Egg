731d2bf28624ed49e39e027015f4b97d3665b986
fanaticscripter <fanaticscripter@gmail.com>
2021-06-27 18:28:28 +0800

    ui: BaseErrorBoundary: adjust text size and space after alert icon

--- ui/components/BaseErrorBoundary.vue
+++ ui/components/BaseErrorBoundary.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="error">
-    <div class="text-center break-words text-red-500">
-      <svg class="relative inline h-4 w-4 -top-px mr-1" viewBox="0 0 20 20" fill="currentColor">
+    <div class="text-center text-sm break-words text-red-500">
+      <svg class="relative inline h-4 w-4 -top-px" viewBox="0 0 20 20" fill="currentColor">
         <path
           fill-rule="evenodd"
           d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"

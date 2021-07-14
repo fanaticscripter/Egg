bb64fcfa6f8d5e1553ef44e291d31f36c0dfab99
fanaticscripter <fanaticscripter@gmail.com>
2021-07-14 12:27:44 +0800

    artifact-sandbox: formatFloat: show numbers >= 1e6 in scientific notation

--- wasmegg/artifact-sandbox/src/lib/utils.ts
+++ wasmegg/artifact-sandbox/src/lib/utils.ts
@@ -1,7 +1,20 @@
 import { trimTrailingZeros } from 'lib';
 
 export function formatFloat(x: number, decimals = 3): string {
-  return trimTrailingZeros(x.toFixed(decimals));
+  if (isNaN(x)) {
+    return 'NaN';
+  }
+  if (x < 0) {
+    return '-' + formatFloat(-x, decimals);
+  }
+  if (!isFinite(x)) {
+    return 'infinity';
+  }
+  if (x < 1e6) {
+    return trimTrailingZeros(x.toFixed(decimals));
+  } else {
+    return x.toExponential(decimals);
+  }
 }
 
 export function formatPercentage(x: number, decimals?: number): string {

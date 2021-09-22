499291e05279116a84bcb07d67f6aed0b3db4497
fanaticscripter <fanaticscripter@gmail.com>
2021-09-22 22:44:43 +0800

    *: update to 1.22

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/avast/retry-go v3.0.0+incompatible
-	github.com/fanaticscripter/EggContractor v0.0.0-20210813151010-54e96f233497
+	github.com/fanaticscripter/EggContractor v0.0.0-20210922143229-808c77d03657
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
 	google.golang.org/protobuf v1.25.0
--- lib/api/version.ts
+++ lib/api/version.ts
@@ -1,7 +1,7 @@
 import { aux } from '../proto';
 
-export const APP_VERSION = '1.21.2';
-export const APP_BUILD = '1.21.2.1';
-export const CLIENT_VERSION = 34;
+export const APP_VERSION = '1.22';
+export const APP_BUILD = '1.22.0.1';
+export const CLIENT_VERSION = 35;
 export const PLATFORM_STRING = 'IOS';
 export const PLATFORM = aux.Platform.IOS;

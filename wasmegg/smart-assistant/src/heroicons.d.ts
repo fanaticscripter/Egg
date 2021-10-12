2a04250b62c160520d05d63b94e2fba7c6a37af6
fanaticscripter <fanaticscripter@gmail.com>
2021-10-12 23:43:53 +0800

    *: move @headlessui/vue and @heroicons/vue to root package.json
    I plan to use them in shared ui/ code.
    
    Also remove custom heroicons.d.ts declarations. The type declarations have been
    fixed by 1.0.4.

--- wasmegg/smart-assistant/src/heroicons.d.ts
+++
@@ -1,9 +0,0 @@
-// @heroicons/vue does not have type declarations.
-// https://github.com/tailwindlabs/heroicons/issues/252
-// https://github.com/tailwindlabs/heroicons/pull/254
-declare module '@heroicons/vue/solid' {
-  import { DefineComponent } from 'vue';
-  export let BanIcon: DefineComponent;
-  export let CheckIcon: DefineComponent;
-  export let SelectorIcon: DefineComponent;
-}

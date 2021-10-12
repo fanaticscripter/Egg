beaa8009b9f013228f6c6ee4c1bf2f9765aea438
fanaticscripter <fanaticscripter@gmail.com>
2021-10-12 23:21:01 +0800

    eicoop: BaseModal: center modal on phone viewports
    Previously I was using the TailwindUI default where the modal is closer to the
    bottom on mobile. According to a dev when I raised the issue, this is for
    reachability, but I found it to be easily obscured by the virtual keyboard, at
    least on iOS.

--- eicoop/src/components/BaseModal.vue
+++ eicoop/src/components/BaseModal.vue
@@ -8,9 +8,7 @@
       :initial-focus="initialFocus"
       @close="hide"
     >
-      <div
-        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
-      >
+      <div class="min-h-screen text-center">
         <TransitionChild
           as="template"
           enter="ease-out duration-300"
@@ -24,13 +22,11 @@
         </TransitionChild>
 
         <!-- This element is to trick the browser into centering the modal contents. -->
-        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true"
-          >&#8203;</span
-        >
+        <span class="inline-block align-middle h-screen" aria-hidden="true">&#8203;</span>
 
         <TransitionChild
           as="div"
-          class="inline-block align-bottom sm:my-8 sm:align-middle sm:max-w-sm sm:w-full"
+          class="inline-block my-8 align-middle max-w-sm w-full"
           enter="ease-out duration-300"
           enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           enter-to="opacity-100 translate-y-0 sm:scale-100"

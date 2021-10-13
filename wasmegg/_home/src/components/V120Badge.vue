09daeff3aeb83cac082dbca774ce3eddb42c1421
fanaticscripter <fanaticscripter@gmail.com>
2021-10-13 22:42:51 +0800

    wasmegg: _home: mark artifact-explorer as "not *fully* updated..."
    The "not yet updated..." badge is inaccurate since there have been many data
    updates.

--- wasmegg/_home/src/components/V120Badge.vue
+++ wasmegg/_home/src/components/V120Badge.vue
@@ -1,11 +1,22 @@
 <template>
-  <span class="whitespace-nowrap text-sm text-white bg-red-300 px-1.5 py-0.5 rounded-full ml-1">
-    not yet updated for v1.21
+  <span
+    class="whitespace-nowrap text-sm text-white px-1.5 py-0.5 rounded-full ml-1"
+    :class="partiallyDone ? 'bg-orange-300' : 'bg-red-300'"
+  >
+    <template v-if="partiallyDone">not fully updated for v1.21</template>
+    <template v-else>not yet updated for v1.21</template>
   </span>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+export default defineComponent({
+  props: {
+    partiallyDone: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
 </script>
--- wasmegg/_home/tailwind.config.js
+++ wasmegg/_home/tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   mode: 'jit',
-  purge: ['./index.html', './src/**/*.{vue,ts}', '../../ui/**/*.vue'],
+  purge: ['./index.html', './src/**/*.{vue,ts}', '../../ui/**/*.vue', '../../lib/tools/*.ts'],
   theme: {
     fontFamily: {
       sans: ['IBM Plex Sans', ...defaultTheme.fontFamily.sans],
@@ -20,6 +20,7 @@ module.exports = {
       colors: {
         purple: colors.purple,
         fuchsia: colors.fuchsia,
+        orange: colors.orange,
       },
       gridTemplateColumns: {
         'max-1': 'max-content',

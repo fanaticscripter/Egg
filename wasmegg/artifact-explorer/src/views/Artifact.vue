593faadb5d5eb8db68517681579420078a7524da
fanaticscripter <fanaticscripter@gmail.com>
2021-10-18 00:01:32 +0800

    artifact-explorer: warn about outdated data more prominently inline
    Apparently people can't see a big red banner. Not really surprising.

--- wasmegg/artifact-explorer/src/views/Artifact.vue
+++ wasmegg/artifact-explorer/src/views/Artifact.vue
@@ -153,10 +153,29 @@
     <div class="px-4 py-4 sm:px-6 space-y-2">
       <template v-if="!artifact.notDroppableInPractice && obtainableMissions.length > 0">
         <div class="text-sm font-medium text-gray-500">Available from the following missions:</div>
-        <div v-if="showDropRateWarning(artifact)" class="text-xs text-red-600">
-          This item did not drop prior to 2021-05-20 thanks to a server bug, which was fixed
-          "unintentionally" that day. Therefore, the following drop rates may differ from current
-          rates by a lot.
+        <div class="text-xs text-red-600 space-y-1">
+          <p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="inline relative -top-px h-4 w-4"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            Drop rate data have not been updated to post-1.21 (when star levels and capacity epic
+            research were introduced, and drop rates for certain items significantly changed) yet.
+            Absolute values are of limited value at the moment, but most relative comparisons are still valid.
+          </p>
+          <p v-if="showDropRateWarning(artifact)">
+            This item did not drop prior to 2021-05-20 thanks to a server bug, which was fixed
+            "unintentionally" that day. Therefore, the following drop rates may differ from actual
+            rates by a lot.
+          </p>
         </div>
         <ul class="grid grid-cols-1 gap-x-4 gap-y-1 sm:grid-cols-2 xl:grid-cols-3">
           <li
--- wasmegg/artifact-explorer/src/views/Mission.vue
+++ wasmegg/artifact-explorer/src/views/Mission.vue
@@ -18,6 +18,27 @@
     <div class="px-4 py-4 sm:px-6 space-y-2">
       <loot-data-credit />
 
+      <div class="text-xs text-red-600 space-y-1">
+        <p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="inline relative -top-px h-4 w-4"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          Drop rate data have not been updated to post-1.21 (when star levels and capacity epic
+          research were introduced, and drop rates for certain items significantly changed) yet.
+          Absolute values are of limited value at the moment, but most relative comparisons are
+          still valid.
+        </p>
+      </div>
+
       <div class="flex items-center">
         <label for="artifact-sort-by" class="text-xs font-medium text-gray-500 uppercase"
           >Sort by</label

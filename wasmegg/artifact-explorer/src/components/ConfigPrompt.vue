5e2281a5a4d4c9df829560a64e4b78881e148ce4
fanaticscripter <fanaticscripter@gmail.com>
2021-11-23 13:28:28 +0800

    artifact-explorer: ConfigPrompt: make button less pronounced

--- wasmegg/artifact-explorer/src/components/ConfigPrompt.vue
+++ wasmegg/artifact-explorer/src/components/ConfigPrompt.vue
@@ -2,7 +2,7 @@
   <div class="text-xs text-gray-500">Drop rates depend on epic researches and ship levels.</div>
   <button
     type="button"
-    class="flex items-center px-2 py-1 border border-transparent shadow-sm text-xs rounded-md text-white bg-gray-500 hover:bg-gray-600 focus:outline-none sm:focus:ring-2 sm:focus:ring-offset-1 sm:focus:ring-gray-500"
+    class="flex items-center px-2 py-1 border border-gray-300 shadow-sm text-xs rounded-md text-gray-500 bg-gray-100 hover:bg-gray-200 focus:outline-none sm:focus:ring-2 sm:focus:ring-offset-1 sm:focus:ring-gray-300"
     @click="openConfigModal"
   >
     <CogIcon class="-ml-0.5 mr-1 h-4 w-4" aria-hidden="true" />

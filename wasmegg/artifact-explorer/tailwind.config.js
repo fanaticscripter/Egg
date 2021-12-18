13308d85c93b0ba6b49c28b2822cee2260a70a54
fanaticscripter <fanaticscripter@gmail.com>
2021-12-18 14:45:04 +0800

    artifact-explorer: tailwind.config.js: fix custom breakpoint
    A smaller breakpoint added with `extend` is ineffective since breakpoints need
    to sorted from smallest to largest. Larger breakpoint classes aren't
    automatically more specific.
    
    See https://tailwindcss.com/docs/screens#adding-smaller-breakpoints.

--- wasmegg/artifact-explorer/tailwind.config.js
+++ wasmegg/artifact-explorer/tailwind.config.js
@@ -1,11 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   mode: 'jit',
   purge: ['./index.html', './src/**/*.{vue,ts}', '../../ui/**/*.vue'],
   theme: {
+    screens: {
+      xs: '411px',
+      ...defaultTheme.screens,
+    },
     extend: {
-      screens: {
-        xs: '411px',
-      },
       maxWidth: {
         xxxs: '12rem',
         xxs: '16rem',

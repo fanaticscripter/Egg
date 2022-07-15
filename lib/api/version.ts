fe49ff7b958d0bc0e465475479bc9c9e56748a66
fanaticscripter <fanaticscripter@gmail.com>
2022-07-15 21:26:19 +0800

    lib/api/version.ts: update to iOS version 1.23.1

--- lib/api/version.ts
+++ lib/api/version.ts
@@ -1,7 +1,7 @@
 import { ei } from '../proto';
 
-export const APP_VERSION = '1.22.9';
-export const APP_BUILD = '1.22.9.0';
-export const CLIENT_VERSION = 37;
+export const APP_VERSION = '1.23.1';
+export const APP_BUILD = '1.23.1.0';
+export const CLIENT_VERSION = 38;
 export const PLATFORM_STRING = 'IOS';
 export const PLATFORM = ei.Platform.IOS;
